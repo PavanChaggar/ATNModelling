bcbb08e438b21bd8afbe1b99b78fb0dde1d54ff4
Pavan Chaggar <pavanjit.chaggar@exeter.ox.ac.uk>
2024-12-05 18:02:25 +0000

    more docstrings

--- src/parcellation-utils.jl
+++ src/parcellation-utils.jl
@@ -4,14 +4,29 @@ using Connectomes: Parcellation, node2FS, get_lobe, get_node_id, connectome_path
 
 dktdict = node2FS()
 
+"""
+    get_parcellation()
+
+Return a `Connectomes.Parcellation` for the DKT atlas.
+"""
 function get_parcellation()
     Parcellation(connectome_path())
 end
 
+"""
+   get_cortex(parc::Parcellation)
+
+Filter the `parc` for only cortical regions.
+"""
 function get_cortex(parc::Parcellation)
     filter(x -> get_lobe(x) != "subcortex", parc)
 end
 
+"""
+   get_dkt_names(parc::Parcellation)
+
+Generate regional names according to FreeSurfer output
+"""
 function get_dkt_names(parc::Parcellation)
     [dktdict[i] for i in get_node_id.(parc)]
 end
