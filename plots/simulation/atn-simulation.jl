029ede34b5c977e765ca811244f992336e8fff6c
pavanchaggar <pavanchaggar@gmail.com>
2026-06-09 18:26:25 +0200

    updating figures and adding braid diagram code

--- plots/simulation/atn-simulation.jl
+++ plots/simulation/atn-simulation.jl
@@ -33,11 +33,11 @@ ab_inits = copy(u0)
 tau_inits = copy(v0)
 atr_inits = zeros(72)
 
-ab_inits .+= 0.25 .* (ui .- u0)
+ab_inits .+= 0.1 .* (ui .- u0)
 
 tau_seed_regions = ["entorhinal" ]#,"Left-Amygdala", "Right-Amygdala", "Left-Hippocampus", "Right-Hippocampus"]
 tau_seed_idx = findall(x -> get_label(x) ∈ tau_seed_regions, c.parc)
-tau_inits[tau_seed_idx] .+= 0.25 .* ((part[tau_seed_idx] .+ β .* (ui[tau_seed_idx] - u0[tau_seed_idx])) .- v0[tau_seed_idx])
+tau_inits[tau_seed_idx] .+= 0.1 .* ((part[tau_seed_idx] .+ β .* (ui[tau_seed_idx] - u0[tau_seed_idx])) .- v0[tau_seed_idx])
 
 _vi = part .+ (β .* (ui .- u0))
 inits = [ab_inits; tau_inits; atr_inits]
@@ -55,7 +55,10 @@ using CairoMakie; CairoMakie.activate!()
 plot(sol, idxs=1:72)
 plot(sol, idxs=73:144)
 absol = ( Array(sol[1:72,:]) .- u0 ) ./ ( ui .- u0 ) 
-tausol = ( Array(sol[73:144,:]) .- v0 ) ./ ( _vi .- v0 )
+
+tausol = ( Array(sols[1,end][73:144,:]) .- v0 ) ./ ( _vi .- v0 )
+tau_bs_sol = [vec(mean(tausol[roi,:], dims=1)) for roi in rois]
+sortperm([findfirst(x -> x > 0.1, tbs) for tbs in tau_bs_sol])
 
 tau_seed = findall(x -> x >= 0.1, tausol)
 tau_seed_idx = zeros(72, size(sol,2))
@@ -72,24 +75,60 @@ coloc_t = findfirst(x -> x > 0, sum(ab_tau_coloc, dims=1))
 coloc_node = findall(x -> x > 0, ab_tau_coloc[:, coloc_t[2]])
 # dktnames[coloc_node][1]
 sol.t[coloc_t[2]]
+
+# Braid diagram
+sols = [simulate(func, inits, tspan, [α_a, ρ_t, i, β, η]; saveat=0.1) for i in 0.05:0.01:1.0];
+
+sols = [
+    simulate(func, inits, tspan, [α_a, ρ, i, β, η]; saveat=0.1)
+    for i in 0.05:0.01:1.0, ρ in 0.05:0.01:1.0
+];
+params = [
+    i/ρ
+    for i in 0.05:0.01:1.0, ρ in 0.05:0.01:1.0
+]
+tausol = ( Array(sols[1,1][73:144,:]) .- v0 ) ./ ( _vi .- v0 )
+tau_bs_sol = reduce(hcat, [vec(mean(tausol[roi,:], dims=1)) for roi in rois])
+function get_braak_order(sol, v0=v0, vi=_vi)
+    tausol = ( Array(sol[73:144,:]) .- v0 ) ./ (_vi .- v0 )
+    tau_bs_sol = [vec(mean(tausol[roi,:], dims=1)) for roi in rois]
+    if !allequal([t[end] for t in tau_bs_sol] .> 0.01)
+         return 0
+    elseif allequal([t[end] for t in tau_bs_sol] .> 0.01) && tau_bs_sol[1][end] < 0.1
+        return 0 
+    else
+        return sortperm([findfirst(x -> x >= 0.01, tbs) for tbs in tau_bs_sol])
+    end
+end
+braid_matrix = get_braak_order.(sols);
+findall(x -> x == [1, 2, 3, 4, 5], braid_matrix)
+heatmap(log.(params))
+heatmap(braid_matrix .== fill([1, 2, 3, 4, 5], 96, 96))
+d = Dict(unique(braid_matrix) .=> 0:1:4)
+function dmap(arr)
+    return d[arr]
+end
+dmap.(braid_matrix)
+
+heatmap(dmap.(braid_matrix) ./ 4)
 # --------------------------------------------------------------------------------
 # AB data
 # --------------------------------------------------------------------------------
-data_path = datadir("ADNI/UCBERKELEY_AMY_6MM_29Nov2024.csv");
+# data_path = datadir("ADNI/UCBERKELEY_AMY_6MM_29Nov2024.csv");
 
-data_df = CSV.read(data_path, DataFrame);
-fbp_data = filter(x -> x.TRACER == "FBB", data_df);
-dropmissing!(fbp_data, :AMYLOID_STATUS_COMPOSITE_REF)
-abpos_df = filter(x -> x["AMYLOID_STATUS_COMPOSITE_REF"] ∈ [0, 1], fbp_data)
-dktnames = get_parcellation() |> get_cortex |> get_dkt_names;
+# data_df = CSV.read(data_path, DataFrame);
+# fbp_data = filter(x -> x.TRACER == "FBB", data_df);
+# dropmissing!(fbp_data, :AMYLOID_STATUS_COMPOSITE_REF)
+# abpos_df = filter(x -> x["AMYLOID_STATUS_COMPOSITE_REF"] ∈ [0, 1], fbp_data)
+# dktnames = get_parcellation() |> get_cortex |> get_dkt_names;
 
-data = ADNIDataset(abpos_df, dktnames; min_scans=2, reference_region="COMPOSITE_REF", qc=false)
+# data = ADNIDataset(abpos_df, dktnames; min_scans=2, reference_region="COMPOSITE_REF", qc=false)
 
-t_df = CSV.read(projectdir("output/analysis-derivatives/ab-derivatives/FBB/ab-times.csv"), DataFrame)
-dfparams = CSV.read(projectdir("output/analysis-derivatives/ab-derivatives/FBB/ab-params.csv"), DataFrame)
+# t_df = CSV.read(projectdir("output/analysis-derivatives/ab-derivatives/FBB/ab-times.csv"), DataFrame)
+# dfparams = CSV.read(projectdir("output/analysis-derivatives/ab-derivatives/FBB/ab-params.csv"), DataFrame)
 
-ts_idx = t_df.t_idx
-ts = t_df.ab_time
+# ts_idx = t_df.t_idx
+# ts = t_df.ab_time
 # --------------------------------------------------------------------------------
 # Simulation visualisatin
 # --------------------------------------------------------------------------------
@@ -116,33 +155,37 @@ fmm_u0, fmm_ui = load_ab_params(tracer="FMM")
 
 xs = collect(0:0.1:1.8)
 linearmodel(x, p) = p[1] .* x
-# tracer_df = DataFrame(fbb_ui = fbb_ui .- fbb_u0, 
-#                     fbp_ui = fbp_ui .- fbp_u0, 
-#                     fmm_ui = fmm_ui .- fmm_u0, 
-#                     ftp_vi = vi .- part, 
-#                     ro_vi = bf_vi - bf_part)
+tracer_df = DataFrame(fbb_ui = fbb_ui .- fbb_u0, 
+                    fbp_ui = fbp_ui .- fbp_u0, 
+                    fmm_ui = fmm_ui .- fmm_u0, 
+                    ftp_vi = vi .- part, 
+                    ro_vi = bf_vi - bf_part)
 
-# fbb_ftp = lm(@formula(  ftp_vi ~ fbb_ui), df)
-# fbp_ftp = lm(@formula(  ftp_vi ~ fbp_ui), df)
-# fmm_ro  = lm(@formula(  ro_vi ~ fmm_ui), df)
+fbb_ftp = lm(@formula(  ftp_vi ~ fbb_ui), tracer_df)
+fbp_ftp = lm(@formula(  ftp_vi ~ fbp_ui), tracer_df)
+fmm_ro  = lm(@formula(  ro_vi ~ fmm_ui), tracer_df)
 
-# testdf = DataFrame(fbb_ui = LinRange(0.0, 1.2, 100),
-#                    fbp_ui = LinRange(0.0, 1.2, 100),
-#                    fmm_ui = LinRange(0.0, 1.2 ,100))
+fbb_ftp = lm(@formula(ftp_vi ~ 0 + fbb_ui), tracer_df)
+fbp_ftp = lm(@formula(ftp_vi ~ 0 + fbp_ui), tracer_df)
+fmm_ro  = lm(@formula(ro_vi  ~ 0 + fmm_ui), tracer_df)
 
-# fbb_pred = predict(fbb_ftp, testdf, interval=:prediction, level=0.95)
-# fbp_pred = predict(fbp_ftp, testdf, interval=:prediction, level=0.95)
-# fmm_pred = predict(fmm_ro, testdf, interval=:prediction, level=0.95)
+testdf = DataFrame(fbb_ui = LinRange(0.0, 1.5, 100),
+                   fbp_ui = LinRange(0.0, 1.5, 100),
+                   fmm_ui = LinRange(0.0, 1.5 ,100))
 
-fbb_fitted_model = curve_fit(linearmodel, fbb_ui .- fbb_u0, vi .- part, [1.0]);
-println(fbb_fitted_model.param)
-println(rsquared(linearmodel(fbb_ui .- fbb_u0, fbb_fitted_model.param), vi .- part))
-fbp_fitted_model = curve_fit(linearmodel, fbp_ui .- fbp_u0, vi .- part, [1.0])
-println(fbp_fitted_model.param)
-println(rsquared(linearmodel(fbp_ui .- fbp_u0, fbp_fitted_model.param), vi .- part))
-bf_fitted_model = curve_fit(linearmodel, fmm_ui .- fmm_u0, bf_vi .- bf_part, [1.0])
-println(fbp_fitted_model.param)
-println(rsquared(linearmodel(fmm_ui .- fmm_u0, bf_fitted_model.param), bf_vi .- bf_part))
+fbb_pred = predict(fbb_ftp, testdf, interval=:prediction, level=0.95)
+fbp_pred = predict(fbp_ftp, testdf, interval=:prediction, level=0.95)
+fmm_pred = predict(fmm_ro, testdf, interval=:prediction, level=0.95)
+
+# fbb_fitted_model = curve_fit(linearmodel, fbb_ui .- fbb_u0, vi .- part, [1.0]);
+# println(fbb_fitted_model.param)
+# println(rsquared(linearmodel(fbb_ui .- fbb_u0, fbb_fitted_model.param), vi .- part))
+# fbp_fitted_model = curve_fit(linearmodel, fbp_ui .- fbp_u0, vi .- part, [1.0])
+# println(fbp_fitted_model.param)
+# println(rsquared(linearmodel(fbp_ui .- fbp_u0, fbp_fitted_model.param), vi .- part))
+# bf_fitted_model = curve_fit(linearmodel, fmm_ui .- fmm_u0, bf_vi .- bf_part, [1.0])
+# println(fbp_fitted_model.param)
+# println(rsquared(linearmodel(fmm_ui .- fmm_u0, bf_fitted_model.param), bf_vi .- bf_part))
 
 bs = get_braak_regions()
 rois = [findall(x -> get_node_id(x) ∈ b , cortex) for b in bs]
@@ -219,35 +262,38 @@ begin
     cmap = Makie.wong_colors()
     ax = Axis(g3[1,1], xlabel="b.c. Aβ SUVR", ylabel="b.c. Tau SUVR", title="FBB vs FTP", titlesize=25,titlefont=:regular, yticks=0:1:3, xticks=0:0.5:1.5, xlabelsize=25, 
                 ylabelsize=25, xticklabelsize=25, yticklabelsize=25)
-    xlims!(ax, 0, 1.25)
-    ylims!(ax, 0, 3.5)
+    xlims!(ax, 0, 1.45)
+    ylims!(ax, 0, 3.9)
+    band!( ax, testdf.fbb_ui, disallowmissing(fbb_pred.lower),disallowmissing(fbb_pred.upper),   color = (cmap[1], 0.15))
     scatter!(fbb_ui .- fbb_u0, vi .- part, color=alphacolor(cmap[1], 0.5), markersize=15, label="FBB")
-    lines!(xs, linearmodel(xs, fbb_fitted_model.param), color=alphacolor(cmap[1], 0.5), linewidth=5)
+    # lines!(xs, linearmodel(xs, fbb_fitted_model.param), color=alphacolor(cmap[1], 0.5), linewidth=5)
     CairoMakie.text!(ax, 0.2, 0.8, text= L"R^{2} = %$(round(rsquared(linearmodel(fbb_ui .- fbb_u0, fbb_fitted_model.param), vi .- part), sigdigits=2))", align=(:left, :bottom), space=:relative, offset=(-40, 0), fontsize=25)
-
-    # lines!(ax,  testdf.fbb_ui, fbb_pred.prediction, color=(cmap[1], 0.2))
-
-    axislegend(ax, position=:rb, fontsize=30)
+    lines!(ax,  testdf.fbb_ui, fbb_pred.prediction, color=(cmap[1], 0.8), linewidth=5)
+    # axislegend(ax, position=:rb, fontsize=30)
+    
     ax = Axis(g3[1,2], xlabel="b.c. Aβ SUVR", ylabel="b.c. Tau SUVR", title="FBP vs FTP", titlesize=25,titlefont=:regular, yticks=0:1:3, xticks=0:0.5:1.5, xlabelsize=25, 
                 ylabelsize=25, xticklabelsize=25, yticklabelsize=25)
-    xlims!(ax, 0, 1.25)
-    ylims!(ax, 0, 3.5)
+    xlims!(ax, 0, 1.45)
+    ylims!(ax, 0, 3.9)
     hideydecorations!(ax, grid=false, ticks=false)
-    scatter!(fbp_ui .- fbp_u0, vi .- part, color=alphacolor(cmap[2], 0.5), markersize=15, label="FBP")
-    lines!(xs, linearmodel(xs, fbp_fitted_model.param), color=alphacolor(cmap[2], 0.5), linewidth=5)
+    band!( ax, testdf.fbp_ui, disallowmissing(fbp_pred.lower),disallowmissing(fbp_pred.upper),   color = (cmap[1], 0.15))
+    scatter!(fbp_ui .- fbp_u0, vi .- part, color=alphacolor(cmap[1], 0.5), markersize=15, label="FBP")
+    # lines!(xs, linearmodel(xs, fbp_fitted_model.param), color=alphacolor(cmap[2], 0.5), linewidth=5)
     CairoMakie.text!(ax, 0.2, 0.8, text= L"R^{2} = %$(round(rsquared(linearmodel(fbp_ui .- fbp_u0, fbp_fitted_model.param), vi .- part), sigdigits=2))", align=(:left, :bottom), space=:relative, offset=(-40, 0), fontsize=25)
-    # lines!(ax,  testdf.fbp_ui, fbp_pred.prediction, color=(cmap[1], 0.2))
-    axislegend(ax, position=:rb, fontsize=30)
+    lines!(ax,  testdf.fbp_ui, fbp_pred.prediction, color=(cmap[1], 0.8), linewidth=5)
+    # axislegend(ax, position=:rb, fontsize=30)
+
     ax = Axis(g3[1,3], xlabel="b.c. Aβ SUVR", ylabel="b.c. Tau SUVR", title="FMM vs RO948", titlesize=25,titlefont=:regular, yticks=0:1:3, xticks=0:0.5:1.5, xlabelsize=25, 
                 ylabelsize=25, xticklabelsize=25, yticklabelsize=25)
-    xlims!(ax, 0, 1.25)
-    ylims!(ax, 0, 3.5)
+    xlims!(ax, 0, 1.45)
+    ylims!(ax, 0, 3.9)
     hideydecorations!(ax, grid=false, ticks=false)
-    scatter!(fmm_ui .- fmm_u0, bf_vi .- bf_part, color=alphacolor(cmap[3], 0.5), markersize=15, label="FMM")
-    lines!(xs, linearmodel(xs, bf_fitted_model.param), color=alphacolor(cmap[3], 0.5), linewidth=5)
+    band!( ax, testdf.fmm_ui, disallowmissing(fmm_pred.lower),disallowmissing(fmm_pred.upper),   color = (cmap[1], 0.15))
+    scatter!(fmm_ui .- fmm_u0, bf_vi .- bf_part, color=alphacolor(cmap[1], 0.5), markersize=15, label="FMM")
+    # lines!(xs, linearmodel(xs, bf_fitted_model.param), color=alphacolor(cmap[1], 0.5), linewidth=5)
     CairoMakie.text!(ax, 0.2, 0.8, text= L"R^{2} = %$(round(rsquared(linearmodel(fmm_ui .- fmm_u0, bf_fitted_model.param), bf_vi .- bf_part), sigdigits=2))", align=(:left, :bottom), space=:relative, offset=(-40, 0), fontsize=25)
-    # lines!(ax,  testdf.fmm_ui, fmm_pred.prediction, color=(cmap[1], 0.2))
-    axislegend(ax, position=:rb, fontsize=30)
+    lines!(ax,  testdf.fmm_ui, fmm_pred.prediction, color=(cmap[1], 0.8), linewidth=5)
+    # axislegend(ax, position=:rb, fontsize=30)
 
     abcmap = ColorScheme(ab_c);
     taucmap = ColorScheme(tau_c); #reverse(ColorSchemes.RdYlBu);
@@ -373,7 +419,6 @@ begin
     Label(g3[1, 1, TopLeft()], "C", fontsize = 26, font = :bold, padding = (0, 0, 10, 0), halign = :left, tellheight=false, tellwidth=false)
     Label(g2[1, 1, TopLeft()], "D", fontsize = 26, font = :bold, padding = (0, 0, 0, 0), halign = :center, tellheight=false, tellwidth=false)
     Label(g4[1, 1, TopLeft()], "E", fontsize = 26, font = :bold, padding = (0, 0, 0, 0), halign = :left, tellheight=false, tellwidth=false)
-    
     
 end
 save(projectdir("output/plots/simulation/atn-simulation-all.jpeg"), f)
